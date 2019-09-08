8e3586fec57991846b92a0632aa52223a7abf9e5
shipanpan <954841234@qq.com>
2019-09-08 21:18:12 +0800

    修复个人详情页访问403问题

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -10,16 +10,24 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 func Fetcher(url string) ([]byte, error) {
-	resp, err := http.Get(url)
-	defer resp.Body.Close()
+	//防止太频繁被屏蔽
+	time.Sleep(1 * time.Second)
+
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36 LBBROWSER")
+	resp, err := http.DefaultClient.Do(request)
 	if err != nil {
 		return nil, err
 	}
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("wrong request response code:%d",resp.StatusCode)
+		return nil, fmt.Errorf("wrong request response code:%d", resp.StatusCode)
 	}
 	e := determineEncoding(resp.Body)
 	utf8reader := transform.NewReader(resp.Body, e.NewDecoder())
@@ -32,7 +40,7 @@ func Fetcher(url string) ([]byte, error) {
 func determineEncoding(r io.Reader) encoding.Encoding {
 	bytes, err := bufio.NewReader(r).Peek(1024)
 	if err != nil {
-		return  unicode.UTF8
+		return unicode.UTF8
 	}
 	e, _, _ := charset.DetermineEncoding(bytes, "")
 	return e
